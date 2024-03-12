56b6c724074c8150a03c5b523a6d9ee3f6d7ba18
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2024-03-12 20:16:15 +0500

    solved sqrt problem

---
+++ sqrt.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/sqrtx/description/
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        import math as m
+        if x==0:
+            return 0
+        a=x/2
+        for i in range(100):
+            a=0.5*(a+x/a)
+
+        return m.floor(a)
